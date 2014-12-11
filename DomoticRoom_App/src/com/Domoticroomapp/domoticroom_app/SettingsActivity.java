566add3a28b6039c56e86f72324babf0c2934425
AlejandroRondon <rondon_udea@hotmail.com>
2014-12-11 08:28:48 -0500

    Version 0

---
+++ DomoticRoom_App/src/com/Domoticroomapp/domoticroom_app/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.Domoticroomapp.domoticroom_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+//import android.widget.Toast;
+
+public class SettingsActivity extends Activity{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Toast.makeText(this, "onCreate/Help", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_settings);
+	}
+	
+}
+
