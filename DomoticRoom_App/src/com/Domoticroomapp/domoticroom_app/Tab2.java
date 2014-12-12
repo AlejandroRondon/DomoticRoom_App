771c970fecb016aa660692e08d984994ff226759
AlejandroRondon <rondon_udea@hotmail.com>
2014-12-12 12:22:56 -0500

    Version 1.4

---
+++ DomoticRoom_App/src/com/Domoticroomapp/domoticroom_app/Tab2.java
@@ -0,0 +1,20 @@
+package com.Domoticroomapp.domoticroom_app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2 extends Fragment  {
+
+ @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View V = inflater.inflate(R.layout.tab2, container, false);
+
+        return V;
+    }
+}
+	     
