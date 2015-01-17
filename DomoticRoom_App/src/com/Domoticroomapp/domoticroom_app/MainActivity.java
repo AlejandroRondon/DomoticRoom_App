4f38ca0460285139af968c7acd619d5b310210ff
AlejandroRondon <rondon_udea@hotmail.com>
2015-01-17 17:29:22 -0500

    Version 13.3 (begin Bluetooth work)

--- DomoticRoom_App/src/com/Domoticroomapp/domoticroom_app/MainActivity.java
+++ DomoticRoom_App/src/com/Domoticroomapp/domoticroom_app/MainActivity.java
@@ -8,7 +8,6 @@ import ViewPagerManager.ViewPagerAdapter;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
